7218539e3b897a60064bb8462a7e0fab7a367fc3
jigishachavda <jigishachavda1011@gmail.com>
2023-07-03 18:03:06 +0530

    news-review-delivary-page

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3375,6 +3375,44 @@ jquery__WEBPACK_IMPORTED_MODULE_0___default()(".myCheckbox").change(function () 
   }
 });
 
+// delivary page dropdown
+jquery__WEBPACK_IMPORTED_MODULE_0___default()('select').each(function () {
+  var $this = jquery__WEBPACK_IMPORTED_MODULE_0___default()(this),
+    selectOptions = jquery__WEBPACK_IMPORTED_MODULE_0___default()(this).children('option').length;
+  $this.addClass('hide-select');
+  $this.wrap('<div class="select"></div>');
+  $this.after('<div class="custom-select"></div>');
+  var $customSelect = $this.next('div.custom-select');
+  $customSelect.text($this.children('option').eq(0).text());
+  var $optionlist = jquery__WEBPACK_IMPORTED_MODULE_0___default()('<ul />', {
+    'class': 'select-options'
+  }).insertAfter($customSelect);
+  for (var i = 0; i < selectOptions; i++) {
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()('<li />', {
+      text: $this.children('option').eq(i).text(),
+      rel: $this.children('option').eq(i).val()
+    }).appendTo($optionlist);
+  }
+  var $optionlistItems = $optionlist.children('li');
+  $customSelect.click(function (e) {
+    e.stopPropagation();
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()('div.custom-select.active').not(this).each(function () {
+      jquery__WEBPACK_IMPORTED_MODULE_0___default()(this).removeClass('active').next('ul.select-options').hide();
+    });
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()(this).toggleClass('active').next('ul.select-options').slideToggle();
+  });
+  $optionlistItems.click(function (e) {
+    e.stopPropagation();
+    $customSelect.text(jquery__WEBPACK_IMPORTED_MODULE_0___default()(this).text()).removeClass('active');
+    $this.val(jquery__WEBPACK_IMPORTED_MODULE_0___default()(this).attr('rel'));
+    $optionlist.hide();
+  });
+  jquery__WEBPACK_IMPORTED_MODULE_0___default()(document).click(function () {
+    $customSelect.removeClass('active');
+    $optionlist.hide();
+  });
+});
+
 /***/ }),
 
 /***/ "./src/js/parts/app.js":
